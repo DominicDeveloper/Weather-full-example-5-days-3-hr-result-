962a157ef16de5c1ce2dc1af123db7e386e4c7ec
Dominic <98159825+DominicDeveloper@users.noreply.github.com>
2024-08-04 00:53:09 +0500

    Update RetrofitService.kt

--- app/src/main/java/com/asadbek/wheatherexample/retrofit/RetrofitService.kt
+++ app/src/main/java/com/asadbek/wheatherexample/retrofit/RetrofitService.kt
@@ -6,8 +6,9 @@ import retrofit2.http.GET
 
 interface RetrofitService {
 
-    //     // 2.5/forecast?lat=40.478293453610355&lon=71.72109800068034&appid=39b9f5aa8264645a6f5fd95692a9e4c5
-    //forecast/daily?lat=44.34&lon=10.99&cnt=7&appid={API key}
-    @GET("2.5/forecast?lat=40.478293453610355&lon=71.72109800068034&lang=en&appid=39b9f5aa8264645a6f5fd95692a9e4c5")
+    // lat va lon - lokatsiya
+    // lang - til
+    // appid - sizga beriladigan api qoyasizlar
+    @GET("2.5/forecast?lat=40.478293453610355&lon=71.72109800068034&lang=en&appid=api_key")
     fun getData():Call<Wheather>
 }
