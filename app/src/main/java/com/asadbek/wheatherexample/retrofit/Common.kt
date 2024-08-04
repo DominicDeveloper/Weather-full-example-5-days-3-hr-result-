61ad296171d71f71dbc64b623dd6788792158ff4
Dominic <98159825+DominicDeveloper@users.noreply.github.com>
2024-08-05 02:45:49 +0500

    Update Common.kt

--- app/src/main/java/com/asadbek/wheatherexample/retrofit/Common.kt
+++ app/src/main/java/com/asadbek/wheatherexample/retrofit/Common.kt
@@ -1,7 +1,7 @@
 package com.asadbek.wheatherexample.retrofit
 
 object Common {
-    // https://api.openweathermap.org/data/2.5/forecast?lat=40.478293453610355&lon=71.72109800068034&appid=39b9f5aa8264645a6f5fd95692a9e4c5
+    // https://api.openweathermap.org/data/2.5/forecast?lat=40.478293453610355&lon=71.72109800068034&appid=api_key
     val BASE_URL = "https://api.openweathermap.org/data/"
 
     val retrofitService:RetrofitService
